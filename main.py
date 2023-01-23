ef7fc50aeca6212671623cc7f9edb13f22b0f90a
Thibsito <thibault.cimino@gmail.com>
2023-01-23 11:57:39 +0100

    File formatted

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from bottle import route, run
 
 
-@route('/hello')
+@route("/hello")
 def hello():
     return "Hello World!"
 
 
-run(host='localhost', port=8080, debug=True)
+run(host="localhost", port=8080, debug=True)
